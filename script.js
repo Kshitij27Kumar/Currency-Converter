9d6420ad0a5dede7888fbdc35529dff67a7cc6d1
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-03-08 18:09:23 +0530

    Calculate function added

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const currency_one = document.getElementById('currency-one'),
 const rate = document.getElementById('rate'),
   swap = document.getElementById('swap')
 
+calculate = () => {
+  const first_currency = currency_one.value
+  const second_currency = currency_two.value
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${first_currency}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const val = data.rates[second_currency]
+      rate.innerText = `1 ${first_currency}=${val} ${second_currency}`
+      amount_two.value = (amount_one.value * val).toFixed(2)
+    })
+}
 //Event-listeners
 currency_one.addEventListener('change', calculate)
 amount_one.addEventListener('input', calculate)
